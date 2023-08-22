ffdc7ef1691100a7bd8ec08ebf95484d6a0f74e8
darkshadowx <asifmahmud.usage@gmail.com>
2023-08-22 17:49:27 +0600

    59 commit

--- Agronomy/production.py
+++ Agronomy/production.py
@@ -17,7 +17,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Agronomy.middleware.SessionCheckerMiddleware',
+    #'Agronomy.middleware.SessionCheckerMiddleware',
     #'Agronomy.middleware.PreventGoogleOauthMiddleware',
 ]
 
