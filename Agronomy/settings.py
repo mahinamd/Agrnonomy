ada7a6d055c296b2f7e3c1b4c954d630b9aeae68
darkshadowx <asifmahmud.usage@gmail.com>
2023-08-23 18:47:36 +0600

    64 commit

--- Agronomy/settings.py
+++ Agronomy/settings.py
@@ -39,11 +39,12 @@ CSRF_TRUSTED_ORIGINS = sensitive.CSRF_TRUSTED_ORIGINS
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    
     'channels',
     'allauth',
     'allauth.account',
